ad23a9bec3d34e057d31dbc38f2741ca08af246e
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-31 17:09:58 -0400

    Login/logout triggers When logged in, user should be taken to search Logged out users sent straight to home page where they can view site and demo, but not use

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import AccountsUI from '../../AccountsUI.jsx';
 
 export const MainLayout = ({content}) => (
 	<div className="main-layout">
+	<header>
+		<nav>
+			<AccountsUI />
+		</nav>
+	</header>
 		{content}
 	</div>
 )
--- client/routes.jsx
+++ client/routes.jsx
@@ -3,11 +3,37 @@ import {mount} from 'react-mounter';
 
 import {MainLayout} from './layouts/MainLayout.jsx';
 import App from '../App.jsx';
+import Search from '../Search.jsx';
+
+if (Meteor.isClient) {
+	Accounts.onLogin(function() {
+		FlowRouter.go('search');
+	});
+	Accounts.onLogout(function() {
+		FlowRouter.go('home');
+	});
+}
 
 FlowRouter.route('/', {
+	name: 'home',
 	action() {
+		if(Meteor.userId()) {
+			FlowRouter.go('search');
+		}
 		mount(MainLayout, {
 			content: (<App />)
+		})
+	}
+});
+
+FlowRouter.route('/search', {
+	name: 'search',
+	action() {
+		if(!Meteor.userId()) {
+			FlowRouter.go('home');
+		}
+		mount(MainLayout, {
+			content: (<Search />)
 		})
 	}
 });
